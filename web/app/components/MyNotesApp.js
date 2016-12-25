ecfbba7292c390cba0943d98c49943cc5d660177
sandro <sandro.heitz@googlemail.com>
2016-12-25 13:32:35 +0100

    Die erste React-Komponente

---
+++ web/app/components/MyNotesApp.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+class MyNotesApp extends Component {
+
+    render() {
+        return (<div>
+            <h1>MyNotes</h1>
+            <p>Was Sie erledigt haben und was Sie noch erledigen müssen!</p>
+            <p>Grossartig - naja</p>
+        </div>);
+    }
+}
+
+export default MyNotesApp;
--- web/app/index.js
+++ web/app/index.js
@@ -1,12 +1,13 @@
 
-// ES6 import
 import React from 'react';
 import { render } from 'react-dom';
 
-// JSX und einfuegen am Mount-Point 'app'
+// Unsere Komponente
+import MyNotesApp from './components/MyNotesApp.js';
+
+// Die Komponente anwenden
 render(
-    <div>
-        <h1>MyNotes</h1><p>Was Sie erledigt haben und was Sie noch erledigen müssen!</p><p>Grossartig - naja</p>
-    </div>,
+    <MyNotesApp />,
     document.getElementById('app')
 )
+
