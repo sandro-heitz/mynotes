c214bde6b8baa175ed41a9be3e9d6b419e69ea4b
sandro <sandro.heitz@googlemail.com>
2016-12-25 13:16:35 +0100

    React, babel (ES6) einbinden und index.js auf JSX umstellen

--- web/app/index.js
+++ web/app/index.js
@@ -1,1 +1,12 @@
-document.write('<h1>MyNotes</h1><p>Was Sie erledigt haben und was Sie noch erledigen müssen!</p><p>Grossartig - naja</p>');
+
+// ES6 import
+import React from 'react';
+import { render } from 'react-dom';
+
+// JSX und einfuegen am Mount-Point 'app'
+render(
+    <div>
+        <h1>MyNotes</h1><p>Was Sie erledigt haben und was Sie noch erledigen müssen!</p><p>Grossartig - naja</p>
+    </div>,
+    document.getElementById('app')
+)
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,20 +2,26 @@
 const path = require('path');
 const webpack = require('webpack');
 
-// __dirname steht für das Verzeichnis in dem das Skript aufgerufen wurde
-// BUILD_DIR ist unser "output" Verzeichnis
-// APP_DIR ist unser "source" Verzeichnis
 const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR = path.resolve(__dirname, 'app');
 
-// die eigentliche Konfiguration von Webpack
 let config = {
-    // der Entry-Point von dem aus webpack den Abhängigkeits-Graph aufbaut
     entry: APP_DIR + '/index.js',
     output: {
-        // output-Verzeichnis und Bundle-Datei
         path: BUILD_DIR,
         filename:  'bundle.js'
+    },
+    module: {
+        // AENDERUNG
+        // loaders, die unseren Source-Code transformieren
+        // in diesem fall wird babel benutzt um notwendige ES6 transformation vorzunehmen
+        loaders: [
+            {
+                test: /\.js?/,
+                include: APP_DIR,
+                loader: 'babel'
+            }
+        ]
     }
 };
 
