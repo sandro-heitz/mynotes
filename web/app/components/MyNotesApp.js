3e72f809289c6882c026db773c2319921e44f13c
sandro <sandro.heitz@googlemail.com>
2016-12-30 17:31:46 +0100

    Alte Aenderung-Markierungen loeschen

--- web/app/components/MyNotesApp.js
+++ web/app/components/MyNotesApp.js
@@ -10,14 +10,11 @@ const spanStyle = { paddingRight: '10px' };
 class MyNotesApp extends Component {
 
     render() {
-        // AENDERUNG :: bootstrap container und columns
-        // AENDERUNG :: class main
         return (<div className="container-fluid main">
             <div className="row">
-                <div className="col-xs-12 col-sm-2"><h1>{/* AENDERUNG : das Bild */}<img src={image}></img></h1></div>
+                <div className="col-xs-12 col-sm-2"><h1><img src={image}></img></h1></div>
                 <div className="col-xs-12 col-sm-10">
                     <h1>{I18n.t('title')}</h1>
-                    {/* AENDERUNG : glyphicon und style attribute */}
                     <p>
                         <span className="glyphicon glyphicon-asterisk" aria-hidden="true" style={spanStyle}></span>
                         Was Sie erledigt haben und was Sie noch erledigen müssen!
