f0c5455a5883892b576204b483e046e3c39527b1
sandro <sandro.heitz@googlemail.com>
2016-12-30 17:29:25 +0100

    Markieren der Aenderungen

--- web/app/components/MyNotesApp.js
+++ web/app/components/MyNotesApp.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 
+// AENDERUNG
 import { I18n } from 'react-i18nify';
 
 import image from '../images/files/mynotes.png';
@@ -10,14 +11,12 @@ const spanStyle = { paddingRight: '10px' };
 class MyNotesApp extends Component {
 
     render() {
-        // AENDERUNG :: bootstrap container und columns
-        // AENDERUNG :: class main
         return (<div className="container-fluid main">
             <div className="row">
-                <div className="col-xs-12 col-sm-2"><h1>{/* AENDERUNG : das Bild */}<img src={image}></img></h1></div>
+                <div className="col-xs-12 col-sm-2"><h1><img src={image}></img></h1></div>
                 <div className="col-xs-12 col-sm-10">
+                    {/* AENDERUNG : i18n */}
                     <h1>{I18n.t('title')}</h1>
-                    {/* AENDERUNG : glyphicon und style attribute */}
                     <p>
                         <span className="glyphicon glyphicon-asterisk" aria-hidden="true" style={spanStyle}></span>
                         Was Sie erledigt haben und was Sie noch erledigen müssen!
