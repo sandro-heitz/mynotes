e0c27eb2b5d4bf332e36ca1ecbaf8e0c6286cde7
sandro <sandro.heitz@googlemail.com>
2016-12-30 17:36:02 +0100

    mocha only tests (fake tests)

---
+++ web/test/mocha_only_test.js
@@ -0,0 +1,16 @@
+
+import assert from 'assert';
+
+describe('Array', () => {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+    describe('#indexOf()', function() {
+        it('should return 0 on checking for 1', function() {
+            assert.equal(0, [1,2,3].indexOf(1));
+        });
+    });
+});
+
