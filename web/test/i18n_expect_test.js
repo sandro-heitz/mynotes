7c4def25e815289817077a6aa8470b4c44048cd2
sandro <sandro.heitz@googlemail.com>
2016-12-30 17:43:15 +0100

    i18n; getParameterByName mit expect

---
+++ web/test/i18n_expect_test.js
@@ -0,0 +1,40 @@
+
+// https://github.com/mjackson/expect
+
+import expect from 'expect'
+
+import { getParameterByName } from '../app/common/utils.js';
+
+
+describe('getParameterByName', () => {
+    describe('#lang', () => {
+        it ('should return undefined if url is undefined', function() {
+            let res = getParameterByName('lang', undefined);
+            expect(res).toNotExist();
+        });
+        it ('should return undefined if query does not exist', () => {
+            let res = getParameterByName('lang', 'http://test.com');
+            expect(res).toNotExist();
+        });
+        it ('should return undefined if query param does not exist', () => {
+            let res = getParameterByName('lang', 'http://test.com?test=eins');
+            expect(res).toNotExist();
+        });
+        it ('should return the value if query param exists', () => {
+            let res = getParameterByName('lang', 'http://test.com?lang=eins');
+            expect(res).toBe('eins');
+        });
+        it ('should return the value if query param exists (anyhow where is query)', () => {
+            let res = getParameterByName('lang', 'http://test.com?lang=eins&test=zwei');
+            expect(res).toBe('eins');
+        });
+        it ('should return the value if query param exists (anyhow where is query)', () => {
+            let res = getParameterByName('lang', 'http://test.com?test=zwei&lang=eins');
+            expect(res).toBe('eins');
+        });
+        it ('should return the value if query param exists (anyhow where is query)', () => {
+            let res = getParameterByName('lang', 'http://test.com?test=zwei&lang=eins&ende=drei');
+            expect(res).toBe('eins');
+        });
+    });
+});
