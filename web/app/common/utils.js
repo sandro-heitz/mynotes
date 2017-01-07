2a7a28386e56a37bb1edaa0b3a1e2c59c08b4407
sandro <sandro.heitz@googlemail.com>
2017-01-07 15:28:14 +0100

    Das Innenleben unserer Form. "ref", "onChange" und Arrow-Funktion in der Klasse.

--- web/app/common/utils.js
+++ web/app/common/utils.js
@@ -11,4 +11,22 @@ let getParameterByName = function(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-export { getParameterByName };
+let toJson = function(obj) {
+    let cache = [];
+    let str = JSON.stringify(obj, function(key, value) {
+        if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                // Circular reference found, discard key
+                return;
+            }
+            // Store value in our collection
+            cache.push(value);
+        }
+        return value;
+    });
+    cache = null;
+    return str;
+}
+
+export { getParameterByName, toJson };
+
--- web/app/components/notes/NoteForm.js
+++ web/app/components/notes/NoteForm.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react';
 import { I18n } from 'react-i18nify';
 
 class NoteForm extends Component {
+    componentDidMount() {
+        this._title.focus();
+    }
+    _onChange = (e) => {
+        // e.target.value
+        console.log(this);
+    }
     render() {
         return (<div className="note-form">
             <form>
                 <div className="form-group">
                     <label >{I18n.t('form.title')}</label>
-                    <input autoFocus type="text" className="form-control"/>
+                    <input ref={node => { this._title = node }}
+                           onChange={this._onChange}
+                           type="text"
+                           className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label>{I18n.t('form.description')}</label>
