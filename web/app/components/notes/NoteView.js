c83cee63f74f58564ef8d498814a6b77edbfc382
sandro <sandro.heitz@googlemail.com>
2017-01-28 13:05:53 +0100

    ref jetzt korrekt mit callback

--- web/app/components/notes/NoteView.js
+++ web/app/components/notes/NoteView.js
@@ -5,19 +5,19 @@ import { I18n } from 'react-i18nify';
 
 class NoteView extends Component {
     componentDidMount() {
-        Velocity(this.refs.noteView,
+        Velocity(this.noteView,
             {  opacity: 1, backgroundColorGreen: 255, backgroundColorRed: 255, backgroundColorBlue: 255 },
             { duration: 1000  } );
     }
     componentWillUnmount() {
-        Velocity(this.refs.noteView, "stop", true);
+        Velocity(this.noteView, "stop", true);
     }
     render() {
         let desc = this.props.note.description;
         desc = desc.split('\n').join('<br />');
         let style = { opacity: '0', backgroundColor: '#FF8533'};
         //
-        return (<div className="note-view" ref="noteView" style={style}>
+        return (<div className="note-view" ref={(n) => { this.noteView = n }} style={style}>
             <h3>{this.props.note.title}</h3>
             <div className="row">
                 <div className="hidden-xs col-sm-4">
