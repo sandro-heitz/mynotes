9032ee0f64a9dce41f1acef334437b90eab7a8e3
sandro <sandro.heitz@googlemail.com>
2017-03-22 13:15:54 +0100

    webpack 2

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -16,12 +16,12 @@ console.log("arg server [" + server + "]");
 let config = {
     resolve: {
         // sicherstellen dass Webpack imported Dateien entsprechend sucht und auch finden kann
-        extensions: ['', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx']
     },
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
+        new (webpack.optimize.OccurenceOrderPlugin || webpack.optimize.OccurrenceOrderPlugin)(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin()
     ],
     entry: [
         'whatwg-fetch',
@@ -35,19 +35,18 @@ let config = {
         filename:  'bundle.js'
     },
     module: {
-        loaders: [
-            // AENDERUNG : bilder die wir nicht inline haben wollen
-            { test: /\.(jpg|png)$/, loader: 'file-loader?name=./images/[name].[hash].[ext]', include: STD_IMAGES_DIR },
-            { test: /\.(jpg|png)$/, loader: 'url-loader?limit=25000', include: INLINE_IMAGES_DIR },
-            { test: /\.css$/, include: CSS_DIRS, loader: "style-loader!css-loader" },
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, loader: "file" },
-            {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, loader: "url?limit=10000&mimetype=application/font-woff" },
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, loader: "url?limit=10000&mimetype=application/octet-stream" },
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, loader: "url?limit=10000&mimetype=image/svg+xml" },
+        rules: [
+            {test: /\.(jpg|png)$/, use: 'file-loader?name=./images/[name].[hash].[ext]', include: STD_IMAGES_DIR },
+            {test: /\.(jpg|png)$/, use: 'url-loader?limit=25000', include: INLINE_IMAGES_DIR },
+            {test: /\.css$/, include: CSS_DIRS, use: [ "style-loader", "css-loader" ] },
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, use: "file-loader" },
+            {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, use: "url-loader?limit=10000&mimetype=application/font-woff" },
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, use: "url-loader?limit=10000&mimetype=application/octet-stream" },
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, include: CSS_DIRS, use: "url-loader?limit=10000&mimetype=image/svg+xml" },
             {
                 test: /\.js?/,
                 include: APP_DIR,
-                loader: 'babel'
+                use: 'babel-loader'
             }
         ]
     },
